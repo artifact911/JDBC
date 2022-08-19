3f293ad3b7c61ca0b0807d6fad1b2eb2b7f8f0e8
artifact911 <artifact911@gmail.com>
2022-08-19 15:39:44 +0300

    ResultSet. Select - done

--- src/main/java/com/artifact/jdbc/starter/JdbcRunner.java
+++ src/main/java/com/artifact/jdbc/starter/JdbcRunner.java
@@ -3,6 +3,7 @@ package com.artifact.jdbc.starter;
 import com.artifact.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcRunner {
@@ -11,20 +12,28 @@ public class JdbcRunner {
         Class<Driver> driverClass = Driver.class;
 
         String sql = """
-                update game.info 
-                set data = 'TestTest'
-                where id = 5
-                returning *
-               
+               select *
+               from ticket
                 """;
 
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
+             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
 
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-            final var executeResult = statement.execute(sql);
-            System.out.println(executeResult);
-            System.out.println(statement.getUpdateCount());
+
+            // ResultSet может быть использован не только для SELECT. Для этого в конструктор Statement нужно передать
+            // connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE), т.к. по-умолчания это ReadOnly.  ->
+            // -> Плохая практика.
+            // ResultSet должен быть закрыт, как и Statement. Но в доке написано, что он будет закрыт автоматически, как только закроется
+            // Statement, который его открыл.
+            var executeResult = statement.executeQuery(sql);
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("passenger_no"));
+                System.out.println(executeResult.getBigDecimal("cost"));
+                System.out.println("---------");
+            }
         }
     }
 }
