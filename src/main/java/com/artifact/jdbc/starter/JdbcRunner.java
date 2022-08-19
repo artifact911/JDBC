bc9f771d67c1bedbaf284d15b4bd035a1b5c8f76
artifact911 <artifact911@gmail.com>
2022-08-19 14:58:33 +0300

    DDL operation example done

--- src/main/java/com/artifact/jdbc/starter/JdbcRunner.java
+++ src/main/java/com/artifact/jdbc/starter/JdbcRunner.java
@@ -10,8 +10,19 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
 
-        try (var connection = ConnectionManager.open()) {
+        String sql = """
+                create table if not exists game.info (
+                    id serial primary key,
+                    data text not null 
+                    );
+                """;
+
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+
             System.out.println(connection.getTransactionIsolation());
+            final var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
