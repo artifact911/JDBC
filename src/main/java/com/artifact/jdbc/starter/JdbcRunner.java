b22964db99e9030e20563358a935e531f048f380
artifact911 <artifact911@gmail.com>
2022-08-21 19:26:45 +0300

    PrepareStatement done

--- src/main/java/com/artifact/jdbc/starter/JdbcRunner.java
+++ src/main/java/com/artifact/jdbc/starter/JdbcRunner.java
@@ -4,29 +4,63 @@ import com.artifact.jdbc.starter.util.ConnectionManager;
 import lombok.SneakyThrows;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-        String flightId = "2";
-        String flightIdHack = "2 OR '' = ''; DROP TABLE game.info";
-        System.out.println(getTicketsByFlightIs(flightIdHack));
+//        Long flightId = 2L
+//        var result = getTicketsByFlightIs(flightId);
+//        System.out.println(result);
 
+        var result = getFlightsBetween(LocalDate.of(2020, 1, 1).atStartOfDay(), LocalDateTime.now());
+        System.out.println(result);
     }
 
     @SneakyThrows
-    private static List<Long> getTicketsByFlightIs(String flightId) {
+    private static List<Long> getFlightsBetween (LocalDateTime start, LocalDateTime end) {
+        String sql = """
+                select id
+                from flight
+                where departure_date between ? and ?
+                """;
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+
+            var resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
+        }
+        return result;
+    }
+
+    @SneakyThrows
+    private static List<Long> getTicketsByFlightIs(Long flightId) {
         String sql = """
                 select id
                 from ticket
-                where flight_id = %s
-                """.formatted(flightId);
+                where flight_id = ?
+                """;
         List<Long> result = new ArrayList<>();
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
-            var resultSet = statement.executeQuery(sql);
+             var preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, flightId);
+
+            var resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
                 // getObject позволяет нам, в случает если придет null получиль его безопасно и распрарсить в обертку Long, т.к.
